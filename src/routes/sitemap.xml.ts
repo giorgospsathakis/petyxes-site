213d6be5effdba3149e82149d0b72792663815ee
giorgospsathakis <giorgospsathakis@gmail.com>
2026-08-20 13:31:25 +0300

    Update sitemap.xml.ts

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -15,7 +15,7 @@ const entries: SitemapEntry[] = [
   { path: "/epikoinonia", changefreq: "monthly", priority: "0.7" },
 ];
 
-export const Route = createFileRoute("/sitemap/xml")({
+export const Route = createFileRoute("/sitemap.xml")({
   server: {
     handlers: {
       GET: async ({ request }) => {
